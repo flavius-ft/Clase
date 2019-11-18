d9a8eef86de0a8c138fc1b81d6bb1ca1ca5f29d9
flavius-ft <56594731+flavius-ft@users.noreply.github.com>
2019-11-18 13:58:34 +0200

    remove last team

--- ClaseBubleSort/SoccerTeamsTests.cs
+++ ClaseBubleSort/SoccerTeamsTests.cs
@@ -10,13 +10,12 @@ namespace ClaseBubleSort
         [Fact]
         public void TeamRanking()
         {
-            SoccerTeamsRanking[] teamRank = new SoccerTeamsRanking[6];
+            SoccerTeamsRanking[] teamRank = new SoccerTeamsRanking[5];
             teamRank[0] = new SoccerTeamsRanking("team1", 30);
             teamRank[1] = new SoccerTeamsRanking("team2", 28);
             teamRank[2] = new SoccerTeamsRanking("team3", 27);
             teamRank[3] = new SoccerTeamsRanking("team4", 20);
             teamRank[4] = new SoccerTeamsRanking("team5", 18);
-            teamRank[5] = new SoccerTeamsRanking("team6", 18);
         }
     }
 }
